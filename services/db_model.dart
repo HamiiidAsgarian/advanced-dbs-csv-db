691ae81376623b5128f03f1d82b40998771b5d0a
Hamid Asgharian <hamiiid.asgarian@gmail.com>
2024-10-02 22:16:53 +0330

    Make models

--- services/db_model.dart
+++ services/db_model.dart
@@ -11,29 +11,43 @@ class MyCsvDb {
 
   //Create DB
 
-  create({required List<List<String>> modelData}) async {
+  create({required List<List<String>> dataModel}) async {
     try {
       final file = File(_filePath);
       if (!await file.exists()) {
         // await MyCsvDb(dbName: 'library_db').create(libraryData);
 
-        String csvString = const ListToCsvConverter().convert(modelData);
+        String csvString = const ListToCsvConverter().convert(dataModel);
 
         await file.writeAsString(csvString);
 
-        log('---CSV file created successfully!---');
+        log('---CSV $dbName file created successfully!---');
       }
     } catch (e) {
-      log('---CSV file creation failed!---');
+      log('---CSV $dbName file creation failed!---');
+    }
+  }
+
+  insert({required List<List<String>> data}) async {
+    try {
+      final file = File(_filePath);
+      if (!await file.exists()) {
+        String csvString = const ListToCsvConverter().convert(data);
+
+        await file.writeAsString(csvString);
+
+        log('---CSV $dbName file inserted successfully!---');
+      }
+    } catch (e) {
+      log('---CSV $dbName file inserted failed!---');
     }
   }
 
   //Read DB
-  read() async {
+  Future<List<List<dynamic>>> read() async {
     final file = File(_filePath);
     final csvString = await file.readAsString();
     final data = CsvToListConverter().convert(csvString);
-
-    print(data);
+    return data;
   }
 }
---
+++ test/t1.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('reads CSV file correctly', () async {
+    expect('1', equals('1'));
+  });
+}
